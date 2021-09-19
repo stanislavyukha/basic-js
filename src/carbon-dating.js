571155bac1f191fe873e8f894429b3928a24a8a1
Stanislav Yukha <valhalla245@gmail.com>
2021-09-19 17:24:58 +0300

    feat: solve count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(!sampleActivity || sampleActivity === false || typeof sampleActivity !== 'string' || isNaN(+sampleActivity) || !Number.isFinite(+sampleActivity)) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY/+sampleActivity) / k);
+  if (t === Infinity || t < 0 || isNaN(t)) return false;
+  return t;
+  
 }
+
+
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,12 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let catsCount = 0;
+  matrix.forEach(el => {
+    catsCount += el.filter((el) => {
+      return el === '^^';
+    }).length;
+  })
+  return catsCount;
 }
