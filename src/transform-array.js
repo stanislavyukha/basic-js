a84a8a22c535e77fc5d00bc45e0f3f3e03427a1c
Stanislav Yukha <valhalla245@gmail.com>
2021-09-20 13:10:15 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,79 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if(!Array.isArray(arr) || typeof arr === 'number') {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if(arr.length === 0) {
+    return [];
 }
+
+  let result = arr.slice();
+
+  if (result.indexOf('--double-next') >= 0) {
+      let index = result.indexOf('--double-next');
+    if(result[index+1] !== undefined) {
+        result[index] = result[index+1];
+    } else {
+      result.pop();
+    }
+  }
+
+  
+  if (result.indexOf('--double-prev') >= 0) {
+    let index = result.indexOf('--double-prev');
+    if(result[index-1] !== undefined && result[index-2] !== '--discard-next'){
+      result[index] = result[index-1];
+    } else if (result[index-2] === '--discard-next') {
+      result[index-2] = 0;
+      result[index-1] = 0;
+      result[index] = 0;
+      result = result.filter(el => el !== 0);
+    }
+     else {
+      result.shift();
+    }
+    
+
+  }
+
+  if (result.indexOf('--discard-prev') >= 0) {
+    let index = result.indexOf('--discard-prev');
+    if(result[index-2] !== '--discard-next') {
+      let partBefore = [];
+      let partAfter = [];
+      for(let i=0; i < index-1; i++) {
+        partBefore.push(result[i]);
+      }
+      for(let i=index+1; i < result.length; i++) {
+        partAfter.push(result[i]);
+      }
+      result = partBefore.concat(partAfter);
+    } else {
+      result[index-2] = 0;
+      result[index-1] = 0;
+      result[index] = 0;
+      result = result.filter(el => el !== 0);
+    }
+    
+  }
+
+  if (result.indexOf('--discard-next') >= 0) {
+    let index = result.indexOf('--discard-next');
+    let partBefore = [];
+    let partAfter = [];
+    for(let i=0; i < index; i++) {
+      partBefore.push(result[i]);
+    }
+    for(let i=index+2; i < result.length; i++) {
+      partAfter.push(result[i]);
+    }
+    result = partBefore.concat(partAfter);
+  }
+ return result;
+
+}
+
+
+
