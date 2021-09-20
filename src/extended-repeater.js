41cd1d00d455d5c05e79d56ff308fb2870312a64
Stanislav Yukha <valhalla245@gmail.com>
2021-09-20 21:06:06 +0300

    feat: solve st-common-character and extended-repeater tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -11,11 +11,77 @@ import { NotImplementedError } from '../extensions/index.js';
  * @example
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+  addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, { repeatTimes, separator, addition, additionRepeatTimes, additionSeparator }) {
+
+  if(typeof str !== 'string') {
+    str += '';
+  }
+  if(typeof addition !== 'string' && addition !== undefined) {
+    addition += '';
+  }
+  let result = str;
+  if(addition) {
+    if(additionRepeatTimes) {
+      let additionPart = '';
+      let separatorAddition = additionSeparator || '|';
+      if(additionRepeatTimes >1 ) {
+        for(let i = 0; i <additionRepeatTimes; i++ ) {
+          if(i === additionRepeatTimes - 1) {
+            additionPart += addition;
+          } else {
+            additionPart += addition + separatorAddition;
+          }
+        }
+      } else if (additionRepeatTimes === 1) {
+        additionPart = addition;
+      }
+        result += additionPart;
+     
+    } else {
+      result += addition;
+    }
+  }
+    
+  let separatorMain = separator || '+';
+  let mainPart = result;
+  result = '';
+  if(repeatTimes) {
+    for(let i = 0; i <repeatTimes; i++) {
+      if(i === repeatTimes - 1) {
+        result += mainPart + '';
+      } else {
+        result += mainPart + separatorMain;
+      }
+    }
+  } else {
+    result = mainPart;
+  }
+
+  return result;
+
 }
+
+
+// repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' });
+//'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
+//'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
+
+// repeater('REPEATABLE_STRING',{ repeatTimes: 2, addition: 'ADDITION', additionRepeatTimes: 3 })
+// -REPEATABLE_STRINGADDITION+REPEATABLE_STRINGADDITION
+//'REPEATABLE_STRINGADDITION+REPEATABLE_STRINGADDITION'
+//'+REPEATABLE_STRINGADDITION|ADDITION|ADDITION+REPEATABLE_STRINGADDITION|ADDITION|ADDITION'
+// +REPEATABLE_STRINGADDITION|ADDITION|ADDITION+REPEATABLE_STRINGADDITION|ADDITION|ADDITION
+
+
+
+
+// repeater('la', { repeatTimes: 3, separator: 's', addition: '+', additionRepeatTimes: 1 });
+//  'la+sla+sla+';
+
+//'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+//'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+// (STR__Addition * (addition repeat times)__Addition-Separator__Separator ) * (repeat times)
--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  let s2Len = s2.length;
+  for(let i = 0; i < s1.length; i++) {
+    if(s2.includes(s1[i])) {
+      s2 = s2.replace(s1[i], '')
+    }
+  }
+  console.log(s2);
+  return s2Len - s2.length;
 }
+
+
